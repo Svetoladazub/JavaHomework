233e9fce3af10f4453f909b67aa8a9fa4baa45b0
Svetoladazub <128633689+Svetoladazub@users.noreply.github.com>
2023-04-08 21:51:13 +0200

    Home Work 1

--- ScannerNumber2.java
+++ ScannerNumber2.java
@@ -6,12 +6,13 @@ public class ScannerNumber2 {
     public static void main(String[] args) {
 
         // number - целое число
-        // n - степень возведения
+        
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter pls number : ");
         int number = sc.nextInt();
         System.out.println("You´ve entered" + number);
+        
         System.out.print("short way 10->2= " + Integer.toBinaryString(number));
 
 
